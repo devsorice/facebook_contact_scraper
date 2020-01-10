549188e52dc4bcb984ffb225f60a70b1e26c9f7a
devsorice <devsorice@gmail.com>
2020-01-10 21:17:23 +0100

    Delete scraper.bat

--- scraper.bat
+++
@@ -1,4 +0,0 @@
-;$.get('https://www.facebook.com/commentimemorabiliofficial/')
-;C:\Program Files (x86)\Google\Chrome\Application\chrome.exe
-;https://ss64.com/vb/sendkeys.html
-start chrome.exe --disable-web-security --disable-gpu --user-data-dir=~/chromeTemp  --new-window jquery.com
